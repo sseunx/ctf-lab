028740af561ae71d1ffeb47def12e99d414be587
sseunx <79503662+sseunx@users.noreply.github.com>
2026-01-13 16:05:50 -0500

    Delete tools/sql/crack-the-gate-tests.sql

--- tools/sql/crack-the-gate-tests.sql
+++
@@ -1,6 +0,0 @@
--- SQL tests cases used to evaluate authentication logic 
--- No flags or sensitive outputs are documented 
-
--- Example category: 
--- 1. Boolean-based authentiaction checks 
--- 2. Input Validation edge cases 
